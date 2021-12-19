f774c6451e5144415ad8eb4f4339dd8f68a42475
wii <zhenkai.sun@qq.com>
2021-12-19 23:26:36 +0800

    mod sample

--- src/tutorial/sample.cpp
+++ src/tutorial/sample.cpp
@@ -1,13 +1,27 @@
+#include <seastar/core/app-template.hh>
+#include <seastar/core/reactor.hh>
 #include <iostream>
-#include "seastar/core/app-template.hh"
-#include "seastar/core/reactor.hh"
-
-using namespace std;
 
 int main(int argc, char **argv) {
     seastar::app_template app;
-    app.run(argc, argv, [] {
-        std::cout << "Hello world\n";
+    namespace bpo = boost::program_options;
+    app.add_options()
+            ("flag", "some optional flag")
+            ("size,s", bpo::value<int>()->default_value(100), "size");
+    app.add_positional_options({
+                                       {"filename", bpo::value<std::vector<seastar::sstring>>()->default_value({}),
+                                               "sstable files to verify", -1}
+                               });
+    app.run(argc, argv, [&app] {
+        auto& args = app.configuration();
+        if (args.count("flag")) {
+            std::cout << "Flag is on\n";
+        }
+        std::cout << "Size is " << args["size"].as<int>() << "\n";
+        auto& filenames = args["filename"].as<std::vector<seastar::sstring>>();
+        for (auto&& fn : filenames) {
+            std::cout << fn << "\n";
+        }
         return seastar::make_ready_future<>();
     });
     return 0;
