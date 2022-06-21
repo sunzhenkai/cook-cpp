ba4a9a8e18fb2e0e15a2f69f2efabc31f5068346
sunzhenkai <zhenkai.sun@qq.com>
2022-06-21 20:09:01 +0800

    add sample

---
+++ src/sample/simple/parameter_pack.cpp
@@ -0,0 +1,11 @@
+#include "iostream"
+#include <tuple>
+
+template<typename T, T... Ts>
+void f() {
+}
+
+int main() {
+    auto seq = std::make_index_sequence<>
+    return 0;
+}
---
+++ src/sample/simple/tuple.cpp
@@ -0,0 +1,30 @@
+#include <tuple>
+#include "iostream"
+
+typedef std::tuple<int, int> R;
+
+int pow(int v) {
+    return v * v;
+}
+
+template<std::size_t I = 0, typename... Vs>
+inline typename std::enable_if<I == sizeof...(Vs), void>::type PrintPP(std::tuple<Vs...> &t) {
+    std::cout << std::get<I>(t) << std::endl;
+    PrintPP<I + 1, Vs...>(t);
+}
+
+template<size_t... Vs>
+void PrintSeq(std::index_sequence<Vs...> seq) {
+    std::cout << sizeof...(Vs) << std::endl;
+    auto tp = std::make_tuple(pow(Vs)...);
+}
+
+int main() {
+    R r(1, 2);
+    std::cout << std::get<1>(r) << std::endl;
+
+    constexpr size_t nr = std::tuple_size<R>::value;
+    auto seq = std::make_index_sequence<nr>{};
+    PrintSeq(seq);
+    return 0;
+}
