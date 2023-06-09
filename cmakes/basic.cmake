b4f505cc7cbe80b52a0b33f6f52b5a781463f903
zhenkai.sun <zhenkai.sun@qq.com>
2023-06-09 23:21:38 +0800

    mod

--- cmakes/basic.cmake
+++ cmakes/basic.cmake
@@ -1,14 +1,13 @@
 include_directories(${SRC_INCLUDE_DIR})
 
-find_package(fmt REQUIRED)
-find_package(Boost 1.76 COMPONENTS ALL)
+#find_package(fmt REQUIRED)
+#find_package(Boost 1.76 COMPONENTS ALL)
 
 add_executable(basic src/basic/sample_a.cpp)
-add_executable(sample_b src/basic/sample_b.cpp)
-target_link_libraries(sample_b fmt::fmt)
-add_executable(complex_example src/basic/complex_example.cpp)
-add_executable(template src/basic/template.cpp)
-target_link_libraries(template Boost::filesystem)
+#add_executable(sample_b src/basic/sample_b.cpp)
+#target_link_libraries(sample_b fmt::fmt)
+#add_executable(template src/basic/template.cpp)
+#target_link_libraries(template Boost::filesystem)
 
 set(CMAKE_CXX_FLAGS "-O0")
 add_executable(sample_c src/basic/sample_c.cpp)
