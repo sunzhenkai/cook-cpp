0ed1728877b91f4c0c4d386d414cf69a4197d126
zhenkai.sun <zhenkai.sun@qq.com>
2023-06-12 08:38:13 +0800

    mod brpc

--- cmakes/brpc.cmake
+++ cmakes/brpc.cmake
@@ -1,6 +1,12 @@
 include(${PROJECT_SOURCE_DIR}/external/boost/load.cmake)
 include(${PROJECT_SOURCE_DIR}/external/brpc/load.cmake)
+message(STATUS "PKG_CONFIG_PATH=$ENV{PKG_CONFIG_PATH}")
+
+#find_package(brpc CONFIG REQUIRED PATHS /tmp/cpp-external-lib/brpc NO_DEFAULT_PATH)
+pkg_search_module(PKG_BRPC REQUIRED IMPORTED_TARGET libbrpc)
 
 add_executable(sample_brpc src/tutorial/brpc/sample_brpc.cpp)
 add_dependencies(sample_brpc brpc boost)
+#target_link_libraries(sample_brpc libbrpc)
+target_link_libraries(sample_brpc PkgConfig::PKG_BRPC)
 #target_link_libraries(sample_brpc brpc)
