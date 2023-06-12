5764fd7c7506185d5fe91611d7ec41c3b14698a4
zhenkai.sun <zhenkai.sun@qq.com>
2023-06-12 08:21:08 +0800

    mod

--- cmakes/brpc.cmake
+++ cmakes/brpc.cmake
@@ -1,4 +1,6 @@
-include(${PROJECT_SOURCE_DIR}/external/incubator-brpc/load.cmake)
+include(${PROJECT_SOURCE_DIR}/external/boost/load.cmake)
+include(${PROJECT_SOURCE_DIR}/external/brpc/load.cmake)
 
 add_executable(sample_brpc src/tutorial/brpc/sample_brpc.cpp)
-#target_link_libraries(sample_brpc fmt::fmt)
+add_dependencies(sample_brpc brpc boost)
+#target_link_libraries(sample_brpc brpc)
