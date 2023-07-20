57d41752166a173914ab7142101806988c7aa151
zhenkai.sun <zhenkai.sun@qq.com>
2023-07-20 18:29:42 +0800

    fix seastar

--- cmakes/seastar.cmake
+++ cmakes/seastar.cmake
@@ -1,34 +1,7 @@
 # includes
-include(${PROJECT_SOURCE_DIR}/external/fmt/load.cmake)
-include(${PROJECT_SOURCE_DIR}/external/seastar/load.cmake)
 include_directories(${SRC_INCLUDE_DIR})
 
-
 add_executable(sample_seastar src/tutorial/seastar/sample_seastar.cpp)
-add_dependencies(sample_seastar seastar_lib fmt_lib)
 
-#target_link_libraries(sample_seastar /tmp/cpp-external-lib/seastar/lib/libseastar.a)
-#target_link_libraries(sample_seastar PRIVATE PkgConfig::PKG_SEASTAR PkgConfig::PKG_FMT)
-
-#set(EXTRA_LIB seastar::seastar
-#        fmt::fmt
-#        protobuf::protobuf
-#        spdlog::spdlogd
-#        cryptopp::cryptopp
-#        yaml-cpp::yaml-cpp
-#        ${Boost_LIBRARIES}
-#        pthread
-#        stdc++fs
-#        openssl::ssl
-#        openssl::crypto
-#        curl::curl
-#        dl
-#        z)
-#
-## executables
-#set(TARGETS socket metric future server sample)
-#add_definitions(-DSEASTAR_SCHEDULING_GROUPS_COUNT=16)
-#foreach (TARGET IN LISTS TARGETS)
-#    add_executable(seastar_${TARGET} src/sample/seastar/seastar_${TARGET}.cpp)
-#    target_link_libraries(seastar_${TARGET} ${EXTRA_LIB})
-#endforeach ()
+find_package(Seastar REQUIRED)
+target_link_libraries(sample_seastar PRIVATE Seastar::seastar fmt::fmt)
--- src/tutorial/seastar/sample_seastar.cpp
+++ src/tutorial/seastar/sample_seastar.cpp
@@ -1,13 +1,13 @@
-//#include <seastar/core/app-template.hh>
-//#include <seastar/core/reactor.hh>
+#include <seastar/core/app-template.hh>
+#include <seastar/core/reactor.hh>
 #include <iostream>
 #include "fmt/format.h"
 
 int main(int argc, char** argv) {
     std::cout << fmt::format("{}", "yes") << std::endl;
-//    seastar::app_template app;
-//    app.run(argc, argv, [] {
-//        std::cout << "Hello world\n";
-//        return seastar::make_ready_future<>();
-//    });
+    seastar::app_template app;
+    app.run(argc, argv, [] {
+        std::cout << "Hello world\n";
+        return seastar::make_ready_future<>();
+    });
 }
