985521f5fa57c0f9c261e2537c029adb18911e04
zhenkai.sun <zhenkai.sun@qq.com>
2023-08-09 08:00:08 +0000

    add gtest

--- cmakes/gtests.cmake
+++ cmakes/gtests.cmake
@@ -3,19 +3,11 @@ aux_source_directory(gtests/brpc GTEST_SRCS)
 
 #enable_testing()
 find_package(GTest CONFIG REQUIRED)
-find_package(Thrift CONFIG REQUIRED)
 find_package(unofficial-brpc CONFIG REQUIRED)
-find_package(leveldb CONFIG REQUIRED)
-find_package(Protobuf CONFIG REQUIRED)
-find_package(gflags CONFIG REQUIRED)
-message(STATUS "CC - ${Thrift_FOUND} - ${CMAKE_PREFIX_PATH}")
 
 add_executable(gt_main ${GTEST_SRCS})
 target_link_libraries(gt_main PRIVATE
         GTest::gtest GTest::gtest_main GTest::gmock GTest::gmock_main
         unofficial::brpc::brpc-static
-        leveldb::leveldb
-        thrift::thrift
-        gflags::gflags
         )
 #add_test(AllTestsInMain gt_main)
