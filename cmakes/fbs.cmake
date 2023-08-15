d1f2f06af125a83a8dd0bcd570e08afb6eacd67a
zhenkai.sun <zhenkai.sun@qq.com>
2023-08-15 10:42:40 +0000

    fix fbs

--- cmakes/fbs.cmake
+++ cmakes/fbs.cmake
@@ -1,34 +1,12 @@
 find_package(flatbuffers CONFIG REQUIRED)
-file(MAKE_DIRECTORY ${PROJECT_BINARY_DIR}/gen/flatbuffers/cpp)
+include(${PROJECT_SOURCE_DIR}/vcpkg/cmakes/flatbuffers.cmake)
 
-macro(GenerateFlatBuffersMessage target)
-    cmake_parse_arguments(ARG "" "OUTPUT;PATH" "FILES" ${ARGN})
-    file(GLOB T_FB_FILES ${ARG_FILES})
-    # TODO correct target name
-    get_property(flatbuffers_binary TARGET flatbuffers::flatc PROPERTY LOCATION)
-    foreach (I ${T_FB_FILES})
-        execute_process(COMMAND ${flatbuffers_binary} --cpp --scoped-enums --reflect-names --gen-object-api
-                -o ${ARG_OUTPUT} ${I}
-                RESULT_VARIABLE rc)
-        if (NOT "${rc}" STREQUAL "0")
-            message(FATAL_ERROR "[GenerateFlatbuffersMessage] generate ${I} cpp code failed. [message=${rc}]")
-        endif ()
-        execute_process(COMMAND ${flatbuffers_binary} --binary --schema -o ${ARG_OUTPUT} ${I}
-                RESULT_VARIABLE rc)
-        if (NOT "${rc}" STREQUAL "0")
-            message(FATAL_ERROR "[GenerateFlatbuffersMessage] generate ${I} binary failed. [message=${rc}]")
-        endif ()
-    endforeach ()
-    set(${target}_include ${ARG_OUTPUT})
-    file(GLOB ${target}_src ${ARG_OUTPUT}/*.h)
-    file(GLOB ${target}_binary ${ARG_OUTPUT}/*.bfbs)
-    include_directories(${${target}_include})
-endmacro(GenerateFlatBuffersMessage)
-
-GenerateFlatbuffersMessage(fbs_library
-        PATH ${PROJECT_SOURCE_DIR}/src/tutorial/flatbuffers/protos
+GenerateFlatbuffersMessage(
+        SRCS FB_SRCS
+        BINARIES FB_BINARIES
         FILES ${PROJECT_SOURCE_DIR}/src/tutorial/flatbuffers/protos/*.fbs
         OUTPUT ${PROJECT_BINARY_DIR}/gen/flatbuffers/cpp)
 
-add_executable(fbs src/tutorial/flatbuffers/fb.cpp ${fbs_library_src})
+include_directories(${PROJECT_BINARY_DIR}/gen/flatbuffers/cpp)
+add_executable(fbs src/tutorial/flatbuffers/fb.cpp)
 target_link_libraries(fbs flatbuffers::flatbuffers)
