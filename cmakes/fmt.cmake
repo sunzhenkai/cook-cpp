c5429b0b97045a0c0bbd54a0742482187f9828bc
zhenkai.sun <zhenkai.sun@qq.com>
2023-08-17 03:45:20 +0000

    add fmt test

--- cmakes/fmt.cmake
+++ cmakes/fmt.cmake
@@ -3,3 +3,6 @@ find_package(spdlog REQUIRED)
 
 add_executable(tfmt src/sample/fmt/fmt.cpp)
 target_link_libraries(tfmt fmt::fmt spdlog::spdlog)
+
+add_executable(fmt_b src/tutorial/fmt/fmt_b.cpp)
+target_link_libraries(fmt_b fmt::fmt spdlog::spdlog)
