92482c985023856da3b9f0467d95d9d41c52257c
zhenkai.sun <zhenkai.sun@qq.com>
2023-10-07 13:54:40 +0800

    mod

--- src/basic/sample_e.cpp
+++ src/basic/sample_e.cpp
@@ -4,6 +4,19 @@
 #include "chrono"
 #include "thread"
 #include "chrono"
+#include "unordered_set"
+#include "vector"
+
+template<class T>
+void Unique(std::vector<T> &data) {
+    std::unordered_set<T> store;
+    for (auto &item: data) {
+        store.emplace(std::forward<T>(item));
+    }
+    for (size_t i = 0ul; i < store.size(); ++i) {
+        data[i] = std::forward<T>();
+    }
+}
 
 int main() {
     //    std::ofstream f;
@@ -12,12 +25,12 @@ int main() {
     //        f << i << "\u0004\u0004\u0004\u0005\u0005\u0005\n";
     //        std::this_thread::sleep_for(std::chrono::microseconds(100));
     //    }
-    while (true) {
-        auto result = std::getenv("NPS_BATCH_SIZE");
-        std::cout << result << std::endl;
-        std::cout << (result ? "yes" : "no") << std::endl;
-        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-    }
+//    while (true) {
+//        auto result = std::getenv("NPS_BATCH_SIZE");
+//        std::cout << result << std::endl;
+//        std::cout << (result ? "yes" : "no") << std::endl;
+//        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+//    }
 
     return 0;
 }
