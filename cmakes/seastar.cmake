059bac211681338ecd3fcd05d0e40c68f2d1ad1d
zhenkai.sun <zhenkai.sun@qq.com>
2023-11-21 02:26:25 +0000

    add seastar

--- cmakes/seastar.cmake
+++ cmakes/seastar.cmake
@@ -1,7 +1,9 @@
 # includes
 include_directories(${SRC_INCLUDE_DIR})
+find_package(Seastar REQUIRED)
 
 add_executable(sample_seastar src/tutorial/seastar/sample_seastar.cpp)
-
-find_package(Seastar REQUIRED)
 target_link_libraries(sample_seastar PRIVATE Seastar::seastar fmt::fmt)
+
+add_executable(seastar_in_out src/tutorial/seastar/seastar_in_out.cpp)
+target_link_libraries(seastar_in_out PRIVATE Seastar::seastar fmt::fmt)
