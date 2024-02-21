9250ccb9f10d1b46b4430e78e5ea82296e7ac6ab
zhenkai.sun <zhenkai.sun@qq.com>
2024-02-21 10:53:59 +0800

    echo serivce

--- cmakes/socket.cmake
+++ cmakes/socket.cmake
@@ -5,5 +5,5 @@ aux_source_directory(src/study/socket/basic SOCKET_SRCS)
 message(STATUS "socket srcs ${SOCKET_SRCS}")
 set(CMAKE_CXX_FLAGS "-O0")
 
-add_executable(socket_echo_server src/study/socket/echo_server.cpp ${SOCKET_SRCS})
-add_executable(socket_echo_client src/study/socket/echo_client.cpp ${SOCKET_SRCS})
+add_executable(socket_echo_server src/study/socket/echo_service/echo_server.cpp ${SOCKET_SRCS})
+add_executable(socket_echo_client src/study/socket/echo_service/echo_client.cpp ${SOCKET_SRCS})
