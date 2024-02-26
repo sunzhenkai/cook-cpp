7409c4ec44beb4283e83ca34ce945c628a08fd04
zhenkai.sun <zhenkai.sun@qq.com>
2024-02-26 22:46:08 +0800

    stage

--- cmakes/boost.cmake
+++ cmakes/boost.cmake
@@ -1,7 +1,12 @@
 message(STATUS "CMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}")
 # document https://cmake.org/cmake/help/latest/module/FindBoost.html
-find_package(Boost REQUIRED COMPONENTS filesystem )
-message(STATUS "${Boost_LIBRARY_DIRS} - ${Boost_LIBRARY_TARGETS} - ${Boost_INCLUDE_DIRS} - ${Boost_FOUND} - ${Boost_LIBRARIES}")
-include_directories(${Boost_INCLUDE_DIRS})
+find_package(Boost REQUIRED COMPONENTS filesystem)
+message(STATUS "FindBoost: ${Boost_LIBRARY_DIRS} - ${Boost_LIBRARY_TARGETS} - ${Boost_INCLUDE_DIRS} - ${Boost_FOUND} - ${Boost_LIBRARIES}")
+set(BOOST_LIBRARIES Boost::boost Boost::filesystem)
+#include_directories(${Boost_INCLUDE_DIRS})
+
 add_executable(boost_sample src/sample/boost/sample.cpp)
-target_link_libraries(boost_sample )
+target_link_libraries(boost_sample ${BOOST_LIBRARIES})
+
+add_executable(boost_preprocess src/sample/boost/preprocess.cpp)
+target_link_libraries(boost_preprocess ${BOOST_LIBRARIES})
