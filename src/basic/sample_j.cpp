3aa02f9e6114959581aa534b62be385037cf3533
zhenkai.sun <zhenkai.sun@qq.com>
2025-03-05 20:57:39 +0800

    update

--- src/basic/sample_j.cpp
+++ src/basic/sample_j.cpp
@@ -1,6 +1,9 @@
 #include "iostream"
 #include <vector>
 
+#include "boost/algorithm/string.hpp"
+#include "boost/spirit/home/x3.hpp"
+
 /**
  * 自定义类排序
  */
@@ -16,16 +19,37 @@ public:
     }
 };
 
+void sort() {
+  std::vector<ClsSortable> ls = {{1, "2"},
+                                 {1, "1"},
+                                 {3, "1"},
+                                 {3, "3"},
+                                 {2, "1"}};
+  std::sort(ls.begin(), ls.end(), std::greater<>());
+  // 打印排序后的结果
+  for (auto &i: ls) {
+    std::cout << i.a << " " << i.b << std::endl;
+  }
+}
+
+using boost::spirit::x3::char_;
+auto ident = +(char_("a-zA-z_0-9"));
+
+void combine_rule() {
+  std::vector<std::string> feature_columns;
+  auto push = [&](auto &context) {
+    feature_columns.push_back(_attr(context));
+  };
+  auto combine_rule = ident[push] % '#';
+  std::string line = "a#b#c";
+  std::cout << boost::spirit::x3::parse(line.begin(), line.end(), combine_rule) << std::endl;
+  std::cout << feature_columns.size() << std::endl;
+  for (auto &e: feature_columns) {
+    std::cout << "> " << e << std::endl;
+  }
+}
+
 int main() {
-    std::vector<ClsSortable> ls = {{1, "2"},
-                                   {1, "1"},
-                                   {3, "1"},
-                                   {3, "3"},
-                                   {2, "1"}};
-    std::sort(ls.begin(), ls.end(), std::greater<>());
-    // 打印排序后的结果
-    for (auto &i: ls) {
-        std::cout << i.a << " " << i.b << std::endl;
-    }
-    return 0;
+  combine_rule();
+  return 0;
 }
